89ee0d440e67c37b7d38ef028336166f659cf6fa
Dewaeq <dewaeq@protonmail.com>
2023-12-08 17:57:52 +0100

    day 3 part two

--- day3/src/lib.rs
+++ day3/src/lib.rs
@@ -43,3 +43,56 @@ pub fn part_one(input: &str) -> u32 {
 
     s
 }
+
+pub fn part_two(input: &str) -> u32 {
+    let lines = input.lines().collect::<Vec<_>>();
+    let (width, height) = (lines[0].trim().len(), lines.len());
+
+    let chars = input
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect::<String>();
+
+    let mut i = 0;
+    let mut part_numbers = vec![];
+    while i < chars.len() {
+        let (x, y) = (i % width, i / height);
+        if let Some(number) = read_part_number(&chars[i..]) {
+            let number_width = (number.ilog10() + 1) as usize;
+
+            let x_interval = x.saturating_sub(1)..(x + number_width + 1).min(width);
+            if y > 0 && lines[y - 1][x_interval.clone()].contains('*') {
+                let pos = (y - 1) * width
+                    + x_interval.start
+                    + lines[y - 1][x_interval.clone()].find('*').unwrap();
+                part_numbers.push((number, pos));
+            }
+            if lines[y][x_interval.clone()].contains('*') {
+                let pos =
+                    y * width + x_interval.start + lines[y][x_interval.clone()].find('*').unwrap();
+                part_numbers.push((number, pos));
+            }
+            if y < height - 1 && lines[y + 1][x_interval.clone()].contains('*') {
+                let pos = (y + 1) * width
+                    + x_interval.start
+                    + lines[y + 1][x_interval.clone()].find('*').unwrap();
+                part_numbers.push((number, pos));
+            }
+
+            i += number_width;
+        } else {
+            i += 1;
+        }
+    }
+
+    part_numbers.sort_by_key(|x| x.1);
+
+    part_numbers
+        .iter()
+        .filter(|x| part_numbers.iter().filter(|y| y.1 == x.1).count() == 2)
+        .map(|x| x.0)
+        .collect::<Vec<_>>()
+        .chunks(2)
+        .map(|x| x[0] * x[1])
+        .sum()
+}
