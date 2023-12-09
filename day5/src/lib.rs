14d4ba207bc5fc69b9878b0ccf1402feff379f76
Dewaeq <dewaeq@protonmail.com>
2023-12-09 18:05:44 +0100

    part I go brrrr

--- day5/src/lib.rs
+++ day5/src/lib.rs
@@ -1,16 +1,10 @@
-use std::collections::HashMap;
-
 pub fn part_one(input: &str) -> u32 {
     let (seeds_str, maps_str) = input[6..].split_once("seed-to-soil map:").unwrap();
 
-    let mut seeds = HashMap::new();
-
-    seeds_str
+    let mut ss = seeds_str
         .split_whitespace()
         .flat_map(|x| x.parse::<u32>())
-        .for_each(|x| {
-            seeds.insert(x, x);
-        });
+        .collect::<Vec<_>>();
 
     let maps = maps_str
         .split("map:")
@@ -22,21 +16,20 @@ pub fn part_one(input: &str) -> u32 {
         .collect::<Vec<_>>();
 
     for map in maps {
-        let mut new_seeds = seeds.clone();
+        let mut new_ss = ss.clone();
 
         for x in map.chunks(3) {
             let [dest_start, source_start, range]: [u32; 3] = x.try_into().unwrap();
 
-            for i in 0..range {
-                if let Some(_) = seeds.remove(&(source_start + i)) {
-                    new_seeds.insert(dest_start + i, dest_start + i);
-                    new_seeds.remove(&(source_start + i));
+            for (i, s) in ss.iter().enumerate() {
+                if *s >= source_start && *s < source_start + range {
+                    new_ss[i] = *s - source_start + dest_start;
                 }
             }
         }
 
-        seeds = new_seeds;
+        ss = new_ss.clone();
     }
 
-    *seeds.values().min().unwrap()
+    *ss.iter().min().unwrap()
 }
